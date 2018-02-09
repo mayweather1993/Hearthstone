83971968bc61319145555ba2a8426ce6506d78ba
Павел <spoxer@bigmir.net>
2018-02-09 12:35:17 +0200

    forgot about boostingordercontroller XD

--- src/main/java/com/mayweather/hearthstone/controllers/BoostingOrderController.java
+++ src/main/java/com/mayweather/hearthstone/controllers/BoostingOrderController.java
@@ -1,13 +1,13 @@
 package com.mayweather.hearthstone.controllers;
 
+import com.mayweather.hearthstone.domain.BoostingOrder;
 import com.mayweather.hearthstone.services.BoostingOrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/boost")
@@ -22,4 +22,23 @@ public class BoostingOrderController {
         double cost = boostingOrderService.getCost(fromRank, toRank, salaryPerRank);
         return new ResponseEntity<>(cost, HttpStatus.OK);
     }
-}
+    @GetMapping("/getAll")
+    public ResponseEntity<Page<BoostingOrder>> getAll(final Pageable pageable){
+        Page<BoostingOrder> orders = boostingOrderService.findAll(pageable);
+        return new ResponseEntity<>(orders , HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<BoostingOrder> findById(@PathVariable("id")final Long id){
+        BoostingOrder order = boostingOrderService.findById(id);
+        return new ResponseEntity<>(order , HttpStatus.OK);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<BoostingOrder> create(@RequestBody final int salaryPerRank , final int fromRank , final int toRank){
+        BoostingOrder boostingOrder = new BoostingOrder();
+        boostingOrder.setSalaryPerRank(salaryPerRank);
+        boostingOrder.setFromRank(fromRank);
+        boostingOrder.setToRank(toRank);
+        boostingOrderService.save(boostingOrder);
+        return new ResponseEntity<>(boostingOrder , HttpStatus.OK);
+    }
+ }
--- src/main/java/com/mayweather/hearthstone/domain/BoostingOrder.java
+++ src/main/java/com/mayweather/hearthstone/domain/BoostingOrder.java
@@ -2,6 +2,8 @@ package com.mayweather.hearthstone.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
@@ -9,11 +11,13 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serializable;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "order_details")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Data
 public class BoostingOrder extends Order implements Serializable {
 
     @Column(name = "salaryPerRank")
